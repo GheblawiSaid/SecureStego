d66717095bff9223aaecda53b85669a137fd2d60
saidgheblawi <saidgheblawi@yahoo.com>
2024-06-04 01:04:39 +0800

    added the role field in the admin dashboard

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Models\User;
+use Faker\Provider\ar_EG\Text;
+use Filament\Forms;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->label('Name')
+                    ->required()
+                    ->placeholder('Name'),
+                TextInput::make('email')
+                    ->label('Email')
+                    ->email()
+                    ->required()
+                    ->placeholder('Email'),
+                TextInput::make('password')
+                    ->label('Password')
+                    // ->required()
+                    ->visibleOn('create')
+                    ->placeholder('Password')
+                    ->password()
+                    ->autocomplete('new-password'),
+                TextInput::make('username')
+                    ->label('Username')
+                    ->required()
+                    ->placeholder('Username'),
+                Select::make('is_admin')
+                    ->label('Role')
+                    ->options([
+                        '0' => 'User',
+                        '1' => 'Admin',
+                    ])
+                    ->default('0')
+                    ->required(),
+
+
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('username'),
+                TextColumn::make('email')
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
